b8b69e9ddb7d3828d3b848ae6941b548fb9af8cd
percussionfloh <florian.moldaschl@web.de>
2026-03-12 19:15:59 +0100

    setup

---
+++ app/workers/app.config.ts
@@ -0,0 +1,20 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'purple',
+      secondary: 'fuchsia',
+      neutral: 'zinc'
+    }
+  },
+    prose: {
+      accordion: {
+        slots: {
+          root: 'my-5',
+          trigger: 'text-base'
+        }
+      },
+      accordionItem: {
+        base: 'pb-4 text-muted *:first:mt-0 *:last:mb-0 *:my-1.5'
+      }
+    }
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,52 @@
+import { execSync } from 'node:child_process';
 
+function getCurrentCommitSha() {
+    try {
+        return execSync(`git rev-parse --short=7 HEAD`, { encoding: 'utf8' }).toString().trim();
+    } catch {
+        return '';
+    }
+}
+
+const currentCommitSha = getCurrentCommitSha();
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
-    modules: ['@nuxt/content', '@nuxt/ui',
-        [
-            '@nuxtjs/i18n',
-            {
-                strategy: 'prefix_except_default',
-                locales: [
-                    { code: 'de', language: 'de-DE', file: 'de.yaml', dir: 'ltr' },
-                ],
-                defaultLocale: 'de',
-                langDir: 'locales/',
-            },
-        ],
-    ],
+    runtimeConfig: {
+        public: {
+            currentCommitSha,
+        },
+    },
+    modules: ['@nuxt/content', '@nuxt/ui',  '@pinia/nuxt','@nuxtjs/i18n'],
     css: ['~/assets/main.css'],
+    i18n: {
+        strategy: 'prefix_except_default',
+        locales: [
+            { code: 'de', language: 'de-DE', file: 'de.yaml', dir: 'ltr' },
+        ],
+        defaultLocale: 'de',
+        langDir: 'locales/',
+    },
+    colorMode: {
+        preference: 'light',
+    },
+    vite: {
+        worker: {
+            format: 'es',
+        },
+        optimizeDeps: {
+            exclude: ['verovio'],
+        },
+    },
+    ui: {
+        theme: {
+            colors: [
+                'primary',
+                'secondary',
+                'neutral',
+            ],
+        },
+    },
 });
