0c23163f2b7ac7a2047d308014d669ab4f97a0b9
percussionfloh <florian.moldaschl@web.de>
2026-03-12 13:10:21 +0100

    setup

---
+++ app/layouts/default.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { de } from '@nuxt/ui/locale';
+
+useHead({
+    titleTemplate: (titleChunk) => {
+        return titleChunk ? `${titleChunk} | Abortion Buddies` : 'Abortion Buddies';
+    },
+});
+</script>
+
+<template>
+    <UApp :locale="de">
+        <div class="flex flex-col min-h-screen">
+            <TheHeader />
+            <TheNavigation />
+            <main class="py-8">
+                <slot></slot>
+            </main>
+            <TheFooter class="mt-auto" />
+        </div>
+    </UApp>
+</template>
+
+<style>
+html {
+    font-size: 20px;
+}
+
+.bg-pattern {
+    background-color: oklch(47.413% 0.22303 323.903);
+    background-image: url("/bg-pattern.svg");
+}
+</style>
