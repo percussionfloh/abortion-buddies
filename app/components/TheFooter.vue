8f8008f8462f4bbad48e9228ddfc2fcfeb3bf82d
percussionfloh <florian.moldaschl@web.de>
2026-03-12 21:33:30 +0100

    update Footer

--- app/components/TheFooter.vue
+++ app/components/TheFooter.vue
@@ -1,6 +1,5 @@
 <script setup>
 const localePath = useLocalePath();
-
 const { t } = useI18n();
 
 const columns = [
@@ -8,31 +7,34 @@ const columns = [
         label: t('importantLinks'),
         children: [
             {
-                label: t('start'),
-                to: localePath('index'),
-            },
-            {
-                label: t('faq'),
-                to: localePath('faq'),
-            },
-        ],
-    },
-    {
-        label: t('sitemap'),
-        children: [
-            {
                 label: t('legalNotice'),
-                to: localePath('legal-notice'),
+                to: localePath('legalNotice'),
             },
-        ],
-    },
+        ]
+    }
 ];
 </script>
-@@ -41,7 +31,7 @@
+
+
+<template>
+    <UFooter class="bg-pattern">
+        <template #top>
+            <UContainer>
+                <UFooterColumns :columns="columns" :ui="{
+                    list: 'grid grid-cols-2 gap-x-2 space-y-0 mt-3',
+                    link: 'text-white text-base',
+                    label: 'text-white text-lg',
+                }">
                 <template #right>
                         <div>
                             <div class="flex justify-end">
-                                    Halo
+                                <img src="../assets/abortionbuddy.jpg" class="max-h-47.5" alt="Abortion-Buddy-Logo" />
                             </div>
                         </div>
                     </template>
+                </UFooterColumns>
+            </UContainer>
+        </template>
+    </UFooter>
+</template>
+
