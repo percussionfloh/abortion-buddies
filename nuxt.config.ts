d2635b12fb427f8cc4bdefd9277e0c7057cbe097
percussionfloh <florian.moldaschl@web.de>
2026-03-12 12:59:15 +0100

    setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+    compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
+    modules: ['@nuxt/content', '@nuxt/ui',
+        [
+            '@nuxtjs/i18n',
+            {
+                strategy: 'prefix_except_default',
+                locales: [
+                    { code: 'de', language: 'de-DE', file: 'de.yaml', dir: 'ltr' },
+                ],
+                defaultLocale: 'de',
+                langDir: 'locales/',
+            },
+        ],
+    ],
+    css: ['~/assets/main.css'],
+});
